2bedd724becc90fe504d73df9757ac2cb15f832d
alankuo04 <lcckuoo40@gmail.com>
2021-08-17 18:48:36 +0800

    update Makefile and emulator.

--- Makefile
+++ Makefile
@@ -1,15 +1,11 @@
-all:
-	g++ -c elf_function.cpp dis_function.cpp disassembler.cpp readelf.cpp
-	g++ -o readelf elf_function.o readelf.o
-	g++ -o disassembler elf_function.o dis_function.o disassembler.o
 readelf:
 	g++ -c elf_function.cpp readelf.cpp
 	g++ -o readelf elf_function.o readelf.o
 disassembler:
 	g++ -c elf_function.cpp dis_function.cpp disassembler.cpp
 	g++ -o disassembler elf_function.o dis_function.o disassembler.o
-emulator:
+simple_emulator:
 	g++ -c elf_function.cpp dis_function.cpp simple_emulator.cpp
-	g++ -o emulator elf_function.o dis_function.o simple_emulator.o
+	g++ -o simple_emulator elf_function.o dis_function.o simple_emulator.o
 clean:
-	rm -f readelf disassembler emulator readelf.o disassembler.o simple_emulator.o elf_function.o dis_function.o
+	rm -f readelf disassembler simple_emulator readelf.o disassembler.o simple_emulator.o elf_function.o dis_function.o
--- emulator/emulator.cpp
+++ emulator/emulator.cpp
@@ -5,7 +5,13 @@
 #include"my_elf.h"
 using namespace std;
 
-#define MAX_MEMORY_SIZE 1048576
+#define MAX_MEMORY_SIZE 536870908
+
+void show_register(int *temp_register){
+    for(int i=0;i<16;i++){
+        printf("%4s: 0x%08X\n", register_list[i].c_str(), temp_register[i]);
+    }
+}
 
 int main(int argc, char **argv){
     fstream file;
@@ -28,7 +34,7 @@ int main(int argc, char **argv){
         }
     }
     int entry = elf_header->e_entry/4;
-    /*for(int i=entry;i<entry+10;i++){
+    /*for(int i=MAX_MEMORY_SIZE;i>=0;i--){
         printf("%08X\n", memory[i]);
         //cout<<int(memory[elf_header->e_entry+i])<<endl;
     }*/
@@ -39,9 +45,13 @@ int main(int argc, char **argv){
     int temp_register[32]={};
     temp_register[2]=MAX_MEMORY_SIZE-1;
     int signedbit;
-    uint32_t imm;
+    int imm;
     while(getline(cin, next)){
-        printf("%08X\n", memory[pc/4]);
+    //while(pc<=0x12e54){
+        temp_register[0] = 0;
+        show_register(temp_register);
+        printf("%05X: ", pc);
+        printf("%08X ", memory[pc/4]);
         Instruction instruction = {memory[pc/4]};
         switch (instruction.general.opcode)
         {
@@ -53,28 +63,36 @@ int main(int argc, char **argv){
                 {
                 case 0:
                     opcode = "ADD";
+                    temp_register[instruction.R.rd] = temp_register[instruction.R.rs1]+temp_register[instruction.R.rs2];
                     break;
                 case 1:
                     opcode = "SLL";
+                    temp_register[instruction.R.rd] = (temp_register[instruction.R.rs1]<<(temp_register[instruction.R.rs2]&31));
                     break;
                 case 2:
                     opcode = "SLT";
+                    temp_register[instruction.R.rd] = temp_register[instruction.R.rs1]<temp_register[instruction.R.rs2]?1:0;
                     break;
                 case 3:
                     opcode = "SLTU";
+                    temp_register[instruction.R.rd] = (uint32_t)(temp_register[instruction.R.rs1])<(uint32_t)(temp_register[instruction.R.rs2])?1:0;
                     break;
                 case 4:
                     opcode = "XOR";
+                    temp_register[instruction.R.rd] = (temp_register[instruction.R.rs1]^temp_register[instruction.R.rs2]);
                     break;
                 case 5:
                     opcode = "SRL";
+                    temp_register[instruction.R.rd] = ((uint32_t)(temp_register[instruction.R.rs1])>>(temp_register[instruction.R.rs2]&31));
                     break;
                 case 6:
                     opcode = "OR";
+                    temp_register[instruction.R.rd] = (temp_register[instruction.R.rs1]|temp_register[instruction.R.rs2]);
                     break;
                 case 7:
                     opcode = "AND";
-                    break;            
+                    temp_register[instruction.R.rd] = (temp_register[instruction.R.rs1]&temp_register[instruction.R.rs2]);
+                    break;
                 }
                 break;
             case 32:
@@ -82,9 +100,11 @@ int main(int argc, char **argv){
                 {
                 case 0:
                     opcode = "SUB";
+                    temp_register[instruction.R.rd] = temp_register[instruction.R.rs1]-temp_register[instruction.R.rs2];
                     break;
                 case 6:
                     opcode = "SRA";
+                    temp_register[instruction.R.rd] = (temp_register[instruction.R.rs1]>>(temp_register[instruction.R.rs2]&31));
                     break;
                 default:
                     break;
@@ -94,6 +114,7 @@ int main(int argc, char **argv){
                 break;
             }
             printf("%s %s, %s, %s\n", opcode.c_str(), register_list[instruction.R.rd].c_str(), register_list[instruction.R.rs1].c_str(), register_list[instruction.R.rs2].c_str());
+            pc += 4;
             break;
         case 3:     // 0000011 I type
             signedbit = (instruction.I.imm11_0>>11);
@@ -102,23 +123,39 @@ int main(int argc, char **argv){
             {
             case 0:
                 opcode = "LB";
+                int8_t byte;
+                memcpy(&byte, memory+(temp_register[instruction.I.rs1]+imm), sizeof(int8_t));
+                temp_register[instruction.I.rd] = byte;
                 break;
             case 1:
                 opcode = "LH";
+                int16_t half;
+                memcpy(&half, memory+(temp_register[instruction.I.rs1]+imm), sizeof(int16_t));
+                temp_register[instruction.I.rd] = half;
                 break;
             case 2:
                 opcode = "LW";
+                int32_t word;
+                memcpy(&word, memory+(temp_register[instruction.I.rs1]+imm), sizeof(int32_t));
+                temp_register[instruction.I.rd] = word;
                 break;
             case 4:
                 opcode = "LBU";
+                uint8_t u_byte;
+                memcpy(&u_byte, memory+(temp_register[instruction.I.rs1]+imm), sizeof(uint8_t));
+                temp_register[instruction.I.rd] = u_byte;
                 break;
             case 5:
                 opcode = "LHU";
+                uint16_t u_half;
+                memcpy(&u_half, memory+(temp_register[instruction.I.rs1]+imm), sizeof(uint16_t));
+                temp_register[instruction.I.rd] = u_half;
                 break;
             default:
                 break;
             }
-            printf("%s %s, %s, %u\n", opcode.c_str(), register_list[instruction.I.rd].c_str(), register_list[instruction.I.rs1].c_str(), imm);
+            printf("%s %s, %s, %d\n", opcode.c_str(), register_list[instruction.I.rd].c_str(), register_list[instruction.I.rs1].c_str(), imm);
+            pc += 4;
             break;
         case 19:    // 0010011 I type
             signedbit = (instruction.I.imm11_0>>11);
@@ -127,43 +164,55 @@ int main(int argc, char **argv){
             {
             case 0:
                 opcode = "ADDI";
+                temp_register[instruction.I.rd] = temp_register[instruction.I.rs1]+imm;
                 break;
             case 2:
                 opcode = "SLTI";
+                temp_register[instruction.I.rd] = temp_register[instruction.I.rs1]<imm?1:0;
                 break;
             case 3:
                 opcode = "SLTIU";
+                temp_register[instruction.I.rd] = (uint32_t)(temp_register[instruction.I.rs1])<imm?1:0;
                 break;
             case 4:
                 opcode = "XORI";
+                temp_register[instruction.I.rd] = (temp_register[instruction.I.rs1]^imm);
                 break;
             case 6:
                 opcode = "ORI";
+                temp_register[instruction.I.rd] = (temp_register[instruction.I.rs1]|imm);
                 break;
             case 7:
                 opcode = "ANDI";
+                temp_register[instruction.I.rd] = (temp_register[instruction.I.rs1]&imm);
                 break;
             case 1:
                 opcode = "SLLI";
+                temp_register[instruction.I.rd] = (temp_register[instruction.I.rs1]<<(imm&31));
                 break;
             case 5:
-                if((instruction.I.imm11_0>>5) == 0)
+                if((instruction.I.imm11_0>>5) == 0){
                     opcode = "SRLI";
+                    temp_register[instruction.I.rd] = (temp_register[instruction.I.rs1]<<(imm&31));
+                }
                 else{
                     opcode = "SRAI";
-                    imm = imm-(2<<9);
+                    temp_register[instruction.I.rd] = (int)((uint32_t)(temp_register[instruction.I.rs1])>>(imm&31));
                 }
                 break;
             default:
                 break;
             }
-            printf("%s %s, %s, %u\n", opcode.c_str(), register_list[instruction.I.rd].c_str(), register_list[instruction.I.rs1].c_str(), imm);
+            printf("%s %s, %s, %d\n", opcode.c_str(), register_list[instruction.I.rd].c_str(), register_list[instruction.I.rs1].c_str(), imm);
+            pc += 4;
             break;
         case 103:   // 1100111 I type
             signedbit = (instruction.I.imm11_0>>11);
             imm = signedbit?instruction.I.imm11_0-(2<<11):instruction.I.imm11_0;
             opcode = "JALR";
-            printf("%s %s, %s, %u\n", opcode.c_str(), register_list[instruction.I.rd].c_str(), register_list[instruction.I.rs1].c_str(), imm);
+            temp_register[instruction.I.rd] = pc+4;
+            pc = (temp_register[instruction.I.rs1]+imm);
+            printf("%s %s, %s, %d\n", opcode.c_str(), register_list[instruction.I.rd].c_str(), register_list[instruction.I.rs1].c_str(), imm);
             break;
         case 115:   // 1110011 I type
             if(instruction.I.imm11_0 == 0)
@@ -171,6 +220,7 @@ int main(int argc, char **argv){
             else
                 opcode = "EBREAK";
             printf("%s\n", opcode.c_str());
+            pc += 4;
             break;
         case 35:    // 0100011 S type
             signedbit = (instruction.S.imm11_5>>6);
@@ -179,17 +229,27 @@ int main(int argc, char **argv){
             {
             case 0:
                 opcode = "SB";
+                uint8_t byte;
+                memcpy(&byte, (temp_register+instruction.S.rs2), sizeof(uint8_t));
+                memcpy(memory+(temp_register[instruction.S.rs1]+imm), &byte, sizeof(uint8_t));
                 break;
             case 1:
                 opcode = "SH";
+                uint16_t half;
+                memcpy(&half, (temp_register+instruction.S.rs2), sizeof(uint16_t));
+                memcpy(memory+(temp_register[instruction.S.rs1]+imm), &half, sizeof(uint16_t));
                 break;
             case 2:
                 opcode = "SW";
+                uint32_t word;
+                memcpy(&word, (temp_register+instruction.S.rs2), sizeof(uint32_t));
+                memcpy(memory+(temp_register[instruction.S.rs1]+imm), &word, sizeof(uint32_t));
                 break;
             default:
                 break;
             }
-            printf("%s %s, %s, %u\n", opcode.c_str(), register_list[instruction.S.rs2].c_str(), register_list[instruction.S.rs1].c_str(), imm);
+            pc += 4;
+            printf("%s %s, %s, %d\n", opcode.c_str(), register_list[instruction.S.rs2].c_str(), register_list[instruction.S.rs1].c_str(), imm);
             break;
         case 99:    // 1100011 B type
             signedbit = instruction.B.imm12;
@@ -198,44 +258,79 @@ int main(int argc, char **argv){
             {
             case 0:
                 opcode = "BEQ";
+                if(temp_register[instruction.B.rs1]==temp_register[instruction.B.rs2]){
+                    pc += imm;
+                }
+                else
+                    pc += 4;
                 break;
             case 1:
                 opcode = "BNE";
+                if(temp_register[instruction.B.rs1]!=temp_register[instruction.B.rs2]){
+                    pc += imm;
+                }
+                else
+                    pc += 4;
                 break;
             case 4:
                 opcode = "BLT";
+                if(temp_register[instruction.B.rs1]<temp_register[instruction.B.rs2]){
+                    pc += imm;
+                }
+                else
+                    pc += 4;
                 break;
             case 5:
                 opcode = "BGE";
+                if(temp_register[instruction.B.rs1]>=temp_register[instruction.B.rs2]){
+                    pc += imm;
+                }
+                else
+                    pc += 4;
                 break;
             case 6:
                 opcode = "BLTU";
+                if((uint32_t)(temp_register[instruction.B.rs1])<(uint32_t)(temp_register[instruction.B.rs2])){
+                    pc += imm;
+                }
+                else
+                    pc += 4;
                 break;
             case 7:
                 opcode = "BGEU";
+                if((uint32_t)(temp_register[instruction.B.rs1])>=(uint32_t)(temp_register[instruction.B.rs2])){
+                    pc += imm;
+                }
+                else
+                    pc += 4;
                 break;
             default:
                 break;
             }
-            printf("%s %s, %s, %u\n", opcode.c_str(), register_list[instruction.B.rs2].c_str(), register_list[instruction.B.rs1].c_str(), imm);
+            printf("%s %s, %s, %d\n", opcode.c_str(), register_list[instruction.B.rs2].c_str(), register_list[instruction.B.rs1].c_str(), imm);
             break;
         case 55:    // 0110111 U type
             opcode = "LUI";
-            printf("%s %s, %u\n", opcode.c_str(), register_list[instruction.U.rd].c_str(), instruction.U.imm31_12);
+            temp_register[instruction.U.rd] = (instruction.U.imm31_12<<12);
+            printf("%s %s, %d\n", opcode.c_str(), register_list[instruction.U.rd].c_str(), instruction.U.imm31_12);
+            pc += 4;
             break;
         case 23:    // 0010111 U type
             opcode = "AUIPC";
-            printf("%s %s, %u\n", opcode.c_str(), register_list[instruction.U.rd].c_str(), instruction.U.imm31_12);
+            temp_register[instruction.U.rd] = (instruction.U.imm31_12<<12)+pc;
+            printf("%s %s, %d\n", opcode.c_str(), register_list[instruction.U.rd].c_str(), instruction.U.imm31_12);
+            pc += 4;
             break;
         case 111:   // 1101111 J type
             signedbit = instruction.J.imm20;
             imm = signedbit?(instruction.J.imm19_12<<12)+(instruction.J.imm11<<11)+(instruction.J.imm10_1<<1)-(2<<19):(instruction.J.imm19_12<<12)+(instruction.J.imm11<<11)+(instruction.J.imm10_1<<1);
             opcode = "JAL";
-            printf("%s %s, %u\n", opcode.c_str(), register_list[instruction.J.rd].c_str(), imm);
+            temp_register[instruction.J.rd] = pc + 4;
+            pc += imm;
+            printf("%s %s, %d\n", opcode.c_str(), register_list[instruction.J.rd].c_str(), imm);
             break;        
         default:
             break;
         }
-        pc+=4;
     }
 }
