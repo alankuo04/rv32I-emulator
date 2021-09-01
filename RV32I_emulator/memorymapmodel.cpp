60746a2338b838bdeeee028189c14a00afdf47d5
alankuo04 <68884383+alankuo04@users.noreply.github.com>
2021-09-01 16:59:29 +0800

    showing 32 rows of memory value and setting startIndex of memory

--- RV32I_emulator/memorymapmodel.cpp
+++ RV32I_emulator/memorymapmodel.cpp
@@ -4,6 +4,7 @@ MemoryMapModel::MemoryMapModel(QObject *parent)
     : QAbstractTableModel(parent)
 {
     memory = new uint32_t[MAX_MEMORY_SIZE];
+    startIndex = 0;
 }
 
 void MemoryMapModel::memoryMapChanged()
@@ -13,26 +14,85 @@ void MemoryMapModel::memoryMapChanged()
 
 int MemoryMapModel::rowCount(const QModelIndex & /*parent*/) const
 {
-   return 16;
+   return 32;
 }
 
 int MemoryMapModel::columnCount(const QModelIndex & /*parent*/) const
 {
-    return 2;
+    return 6;
 }
 
 QVariant MemoryMapModel::data(const QModelIndex &index, int role) const
 {
     if (role == Qt::DisplayRole)
     {
+        int row = startIndex+(rowCount()-index.row()+1);
         if(index.column()==0)
         {
-            return QString("0x")+QString("%1").arg(0+index.row()*4, 8, 16, QLatin1Char('0')).toUpper();
+            return QString("0x")+QString("%1").arg(row*4, 8, 16, QLatin1Char('0')).toUpper();
         }
         else if(index.column()==1)
         {
-            return QString("0x")+QString("%1").arg(memory[index.row()], 8, 16, QLatin1Char('0')).toUpper();
+            return QString("0x")+QString("%1").arg(memory[row], 8, 16, QLatin1Char('0')).toUpper();
+        }
+        else if(index.column()==2)
+        {
+            return QString("0x")+QString("%1").arg((memory[row]>>24) & 0xFF, 2, 16, QLatin1Char('0')).toUpper();
+        }
+        else if(index.column()==3)
+        {
+            return QString("0x")+QString("%1").arg((memory[row]>>16) & 0xFF, 2, 16, QLatin1Char('0')).toUpper();
+        }
+        else if(index.column()==4)
+        {
+            return QString("0x")+QString("%1").arg((memory[row]>>8) & 0xFF, 2, 16, QLatin1Char('0')).toUpper();
+        }
+        else if(index.column()==5)
+        {
+            return QString("0x")+QString("%1").arg(memory[row] & 0xFF, 2, 16, QLatin1Char('0')).toUpper();
         }
     }
     return QVariant();
+}
+
+QVariant MemoryMapModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal)
+    {
+        switch (section) {
+        case 0:
+            return QString("Address");
+        case 1:
+            return QString("Word");
+        case 2:
+            return QString("Byte3");
+        case 3:
+            return QString("Byte2");
+        case 4:
+            return QString("Byte1");
+        case 5:
+            return QString("Byte0");
+        }
+    }
+    return QVariant();
+}
+
+void MemoryMapModel::addStartIndex()
+{
+    if(startIndex <= MAX_MEMORY_SIZE-32)
+        startIndex += 2;
+}
+
+void MemoryMapModel::subStartIndex()
+{
+    if(startIndex >= 0)
+        startIndex -= 2;
+}
+
+void MemoryMapModel::setStartIndex(int index)
+{
+    if(startIndex <= MAX_MEMORY_SIZE && startIndex >= 0)
+    {
+        startIndex = (index/4)>MAX_MEMORY_SIZE-32?MAX_MEMORY_SIZE-32:index/4;
+    }
 }
--- RV32I_emulator/memorymapmodel.h
+++ RV32I_emulator/memorymapmodel.h
@@ -13,10 +13,18 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    void addStartIndex();
+    void subStartIndex();
+    void setStartIndex(int index);
 
     uint32_t *memory;
 public slots:
     void memoryMapChanged();
+
+private:
+    int startIndex;
 };
 
 #endif // MEMORYMAPMODEL_H
